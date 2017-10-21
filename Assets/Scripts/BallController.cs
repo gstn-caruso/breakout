088ae18fd6af02f62e000d6494469aecbc844dc0
Gastón Caruso <gstn.caruso@gmail.com>
2017-10-21 16:16:33 -0300

    Player controller and ball bounding with corner colliders

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	private Rigidbody2D _rb;
+	public float Velocidad = 5f;
+
+	private void Start()
+	{
+		_rb = GetComponent<Rigidbody2D>();
+		StartMoving();
+	}
+
+	private void StartMoving () {
+		_rb.velocity = new Vector3(Random.Range(-1, 2) * Velocidad, -Velocidad);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			var paddleX = other.transform.position.x;
+			var ballX = gameObject.transform.position.x;
+			var offsetX = ballX - paddleX;
+			
+			_rb.velocity = new Vector2((offsetX * 2), 1).normalized * Velocidad;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody2D _rb;
+	public float Velocidad = 15f;
+
+	private void Start()
+	{
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update () {
+		_rb.velocity = new Vector3(Input.GetAxis("Horizontal") * Velocidad, 0);
+	}
+}
